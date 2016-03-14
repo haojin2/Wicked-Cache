a3211c98097df908fb214d61c5a1ecc03f227823
haojin2 <haojin2@illinois.edu>
2016-03-13 19:37:23 -0500

    cache.h

--- cache.h
+++ cache.h
@@ -1,23 +1,33 @@
 // CACHE_H_
-#ifndef CACHE_H
-#define CACHE_H
+#ifndef _CACHE_H_
+#define _CACHE_H_
 #include <vector>
 #include <iostream>
 using namespace std;
-class cache_Base{
+class Cache_Base{
 public:
-	cache_Base(){
+	Cache_Base(){
 		cout<<"Base constructor"<<endl;
 	}
-	cache_Base(size_t size){
-		cout<<"base constructor"<<endl;
+	Cache_Base(size_t size){
+		cout<<size<<" kb"<<endl;
 		entries = vector<unsigned long>(size * 1024, 0);
-				
+		this->size = size;	
+	}
+	Cache_Base(size_t size, size_t way){
+		cout<<size<<" kb with "<<way<<" associative "<<endl;
+		this->way = way;
+		this->size = size;
 	}
 	virtual bool read(size_t address);
 	virtual bool write(size_t address);
-private:
+protected:
 	size_t size;
-	vector<unsigned long> entries;	
-}
+	size_t way;
+	vector<unsigned long> entries;
+	
+};
+class LRU: public Cache_Base{
+	
+};
 #endif
